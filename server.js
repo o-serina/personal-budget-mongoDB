bdae04f00fdc04cc808f85a4974c3f0171cf1905
o-serina <so88.opy@gmail.com>
2024-09-14 15:14:09 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,34 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
-app.use('/', express.static('public'));
+app.use("/", express.static("public"));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out', 
-        budget: 30
-    },
-    {
-        title: 'Rent',
-        budget: 350
-    },
-    {
-        title: 'Grocery',
-        budget: 90
-    },
-  ]
-};
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/budget', (req, res) => {
-    res.json(budget);
+app.get("/budget", (req, res) => {
+  fs.readFile("budget-data.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading JSON file:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
